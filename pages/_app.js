398a7139d29d54c02eb19c8d3ec0c47d55aafe8e
Jonathon <zimdarsj@kenyon.edu>
2023-07-05 16:52:58 -0500

    Changed font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/global.css';
+import { EB_Garamond } from 'next/font/google';
+
+const garamond = EB_Garamond({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }) {
     return (
-        <Component {...pageProps} />
-    )
+        <main className={garamond.className}>
+          <Component {...pageProps} />
+        </main>
+      )
 }
